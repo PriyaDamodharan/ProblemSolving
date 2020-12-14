5d372b6661c0c3211556a1c40a56aa621db9e053
Priya Damodharan <42145081+PriyaDamodharan@users.noreply.github.com>
2020-12-14 09:08:57 +0530

    First and last positions of an element in array

--- firstLastPositions.java
+++ firstLastPositions.java
@@ -1,4 +1,5 @@
-/*Given a sorted array with possibly duplicate elements, the task is to find indexes of first and last occurrences of an element x in the given array. 
+/*
+Given a sorted array with possibly duplicate elements, the task is to find indexes of first and last occurrences of an element x in the given array. 
 Examples: 
 
 Input : arr[] = {1, 3, 5, 5, 5, 5, 67, 123, 125}    
